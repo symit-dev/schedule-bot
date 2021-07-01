d8e74ed1850df3159de50dbe88ca7b12e7b6c5c4
symit-dev <76552519+symit-dev@users.noreply.github.com>
2021-07-01 14:33:53 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,4 +1,7 @@
 import pymongo
+import db
+from time import time
+import os, csv
 
 class Mango:
   def __init__(self):
@@ -12,7 +15,7 @@ class Mango:
     
   def get_all_data(self, json_data):
     return self.collection.find(json_data)
-    
+  
 
   def get_single_data(self, json_data):
     self.return_json = self.collection.find_one(json_data)
@@ -26,9 +29,45 @@ class Mango:
     return self.collection.delete_many(json_data)
 
 
-# db = mango()
-# user = {'name': 'Halil', 'lang': 'Python'}
-# print(db.insert(user))
+# db = Mango()
+# user = {
+#   'chat_id': -1001196505905, 
+#   'msg_id': 37204,
+#   'time':'13:44',
+#   'date':'27/06/2021',
+#   'chats':['-1001196505905','-1001196505905']
+#   }
 
-# client = pymongo.MongoClient('mongodb+srv://bot:sumit12345@cluster0.7hauv.mongodb.net/myFirstDatabase?retryWrites=true&w=majorit')
-# print(client.list_database_names())
+def write_csv_dt(items):
+    print(items)
+    today = int(time())
+    filename = f"schedule-{today}.csv"
+    if not os.path.exists(filename):
+      write_row = True
+    else:
+      write_row  = False
+    with open(filename, 'a', newline='', encoding='utf-8') as csvfile: 
+    # creating a csv writer object 
+        csvwriter = csv.writer(csvfile) 
+        if write_row:
+          csvwriter.writerow(["msg_link","time","date","chats"])   
+        csvwriter.writerows(items)
+    return filename
+
+def get_csv_schedule_file():
+  datas = list()
+  db = Mango()
+  for data in db.get_all_data(None):
+    chat_id = data['chat_id']
+    if isinstance(chat_id, int):
+      chat_id = int(str(chat_id).replace('-100',''))
+    # https://t.me/c/1196505905/37204,16:03,27/06/21,/-1001196505905/-1001196505905
+    msg_link = f"https://t.me/c/{chat_id}/{data['msg_id']}"
+    time_ = data['time']
+    date_ = data['date']
+    chats = '/'+'/'.join(data['chats'])
+    datas.append([msg_link, time_, date_, chats])
+  file_ = write_csv_dt(datas)
+  return file_
+
+# print(get_csv_schedule_file())
